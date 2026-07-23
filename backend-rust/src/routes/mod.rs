96dc73f088e7282e015396c27ff857df23ee1068
orlandotellez <espadachinkinno@gmail.com>
2026-07-23 12:26:06 -0600

    feat(backend-rust): agregar endpoint health para veriicar salud

--- backend-rust/src/routes/mod.rs
+++ backend-rust/src/routes/mod.rs
@@ -1,9 +1,17 @@
-use axum::Router;
+use axum::{Json, Router, routing::get};
+use serde_json::{Value, json};
 
 use crate::{features, shared::state::app_state::AppState};
 
+async fn health() -> Json<Value> {
+    Json(json!({
+        "status": "ok",
+        "timestamp": chrono::Utc::now().to_rfc3339()
+    }))
+}
+
 pub fn create_routes() -> Router<AppState> {
-    Router::new().nest(
+    Router::new().route("/health", get(health)).nest(
         "/api/v1/auth",
         features::auth::presentation::routes::routes(),
     )
